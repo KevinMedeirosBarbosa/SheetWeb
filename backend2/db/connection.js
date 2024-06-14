5d18a6602119c6429585992cc081078543920e34
Kevin <kevinmedeiros2002@gmail.com>
2024-06-14 10:57:07 -0300

    Remove backend2 after renaming to backend

--- backend2/db/connection.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("planilhasWeb", "root", "021202", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
-module.exports = sequelize;
